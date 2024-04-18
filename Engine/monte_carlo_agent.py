dad46ce81fb839384b51014123626999cd8fdd4f
bpreier <bpreier@ncsu.edu>
2024-04-18 01:46:01 -0400

    Very basic monte carlo agent done

--- Engine/monte_carlo_agent.py
+++ Engine/monte_carlo_agent.py
@@ -10,17 +10,22 @@ class MonteCarloAgent(Agent):
         community_cards = board.community
         your_wins = 0
         opponent_wins = 0
-        for _ in range(500):
+        ties = 0
+        simulations = 1000
+        for _ in range(simulations):
             deck = Deck(True, None)
+            cards_to_remove = []
             for card in deck.cards:
                 for card_in_hand in cards_in_hand:
                     if str(card) == str(card_in_hand):
-                        deck.cards.remove(card)
+                        cards_to_remove.append(card)
+            for card in cards_to_remove:
+                deck.cards.remove(card)
             opponent_hand = [deck.top(), deck.top()]
             hand = cards_in_hand + community_cards
             opponent_hand = opponent_hand + community_cards
             randomized_community_cards = []
-            for _ in range(7-len(hand)):
+            for _ in range(5-len(community_cards)):
                 randomized_community_cards.append(deck.top())
             opponent_hand += randomized_community_cards
             hand += randomized_community_cards
@@ -28,8 +33,24 @@ class MonteCarloAgent(Agent):
             opponent_score = MonteCarloAgent.handScore(opponent_hand)
             if hand_score > opponent_score:
                 your_wins +=1
+            elif hand_score == opponent_score:
+                ties +=1
             else:
                 opponent_wins +=1
-        print(f"Your win percentage:", your_wins/500)
-        return Action.CALL
+        your_win_percentage = your_wins/(simulations-ties)        
+        print(f"Your win percentage:", your_win_percentage)
+        print(f"Opponent win percentage:", opponent_wins/(simulations-ties))
+        if(your_win_percentage > 0.6):
+            if(Action.MID_BET in validActions):
+                return Action.MID_BET
+            elif(Action.HIGH_BET in validActions):
+                return Action.LOW_BET
+            else:
+                return Action.ALL_IN
+        elif(your_win_percentage > 0.4):
+            if(Action.CHECK in validActions):
+                return Action.CHECK
+            return Action.CALL
+        else:
+            return Action.FOLD
     
